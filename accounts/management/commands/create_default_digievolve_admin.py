119d78cb2c3632e038a7f7dcdb659e62a06ca11e
Mr. Managwu <39161025+chidoziemanagwu@users.noreply.github.com>
2025-04-10 13:09:31 +0100

    .

--- accounts/management/commands/create_default_digievolve_admin.py
+++ accounts/management/commands/create_default_digievolve_admin.py
@@ -1,7 +1,6 @@
 # accounts/management/commands/create_default_digievolve_admin.py
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
-from accounts.models import DigiEvolveAdmin
+from accounts.models import User
 from django.utils import timezone
 from django.conf import settings
 
@@ -33,7 +32,13 @@ class Command(BaseCommand):
                     'email': DEFAULT_ADMIN['email'],
                     'first_name': DEFAULT_ADMIN['first_name'],
                     'last_name': DEFAULT_ADMIN['last_name'],
-                    'is_staff': False  # No Django admin access
+                    'is_staff': True,  # For Django admin access if needed
+                    'is_student': False,  # Not a student
+                    'user_type': 'admin',
+                    'admin_type': DEFAULT_ADMIN['admin_type'],
+                    'is_active_admin': True,
+                    'is_active': True,
+                    'last_admin_login': timezone.now()
                 }
             )
 
@@ -41,31 +46,25 @@ class Command(BaseCommand):
                 user.set_password(DEFAULT_ADMIN['password'])
                 user.save()
 
-                admin = DigiEvolveAdmin.objects.create(
-                    user=user,
-                    admin_type=DEFAULT_ADMIN['admin_type'],
-                    is_active=True,
-                    last_login=timezone.now()
-                )
-
                 self.stdout.write(
                     self.style.SUCCESS(f'''
 Successfully created default DigiEvolve admin:
 - Username: {DEFAULT_ADMIN['username']}
 - Email: {DEFAULT_ADMIN['email']}
-- Admin Type: {admin.get_admin_type_display()}
-- Name: {admin.full_name}
+- Admin Type: {user.get_admin_type_display()}
+- Name: {user.full_name}
 
-This admin can log in at: /accounts/digievolveadmin/login/
+This admin can log in at: /accounts/login/
 '''))
 
             elif options['reset']:
                 user.set_password(DEFAULT_ADMIN['password'])
+                user.is_student = False  # Ensure this is set to False
+                user.user_type = 'admin'
+                user.admin_type = DEFAULT_ADMIN['admin_type']
+                user.is_active_admin = True
+                user.last_admin_login = timezone.now()
                 user.save()
-
-                admin = DigiEvolveAdmin.objects.get(user=user)
-                admin.last_login = timezone.now()
-                admin.save()
 
                 self.stdout.write(
                     self.style.SUCCESS(f"Reset password for default admin: {DEFAULT_ADMIN['username']}")
